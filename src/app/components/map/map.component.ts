f2420e4d3cf50abc87a936434adb65bd43f731ac
HasanHelvali <hasan.helvali.git@gmail.com>
2024-03-11 14:17:14 +0300

    End

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -40,234 +40,6 @@ import VectorImageLayer from 'ol/layer/VectorImage';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MapComponent extends BaseComponent implements OnInit {
-  //#region
-  // constructor(private httpCLient:CustomHttpClient,private modalComponent: MyModalComponent,
-  //   public locDataService:LocDataService, private listModalComponent:GeometryListModalComponent,spinner:NgxSpinnerService) {
-  //     super(spinner)
-  //     this.locDataService.veriOlusturulduSubject.subscribe((veri)=>{
-  //       console.log("veri geldi");
-  //       this.getGeometryBywkt(veri);----------------------------------------------------------------------------------------------------------------------------
-
-  //     })
-
-  // }
-
-  // data:any=null;
-  // data2:any=null;
-  // _type: Type;
-  // mapİsActive:boolean=false;
-  // map:Map;
-  // _featureMap:Map;
-  // isCheck:boolean=false;
-  // isFeatureMapActive:boolean=false;
-  // onChanges() {
-  //   if(this.isFeatureMapActive==true){
-  //     this.disposeMap(this._featureMap)
-  //   }
-  //   for (const opt in Type) {
-  //       //bir değisiklik yapıldıysa
-  //       this.mapİsActive=true;
-  //       this.disposeMap(this.map);
-  //       this.data=null;
-  //       this.locDataService.data=null;
-  //       this.locDataService.data2=null;
-  //     }
-  //   }
-
-  // }
-  // disposeFeatureMap(){
-  //   if(this.isFeatureMapActive==true){
-  //     this.disposeMap(this._featureMap)
-  //     this.ngOnInit()
-  //   }
-  // }
-  // ngOnInit() {
-  //  this.getMap(Type.Point);
-  // }
-
-  // mapDraw(draw):any{
-  //   //This keyword unu degistirdim.
-  //   let _that=this;-
-  //   this.map.on('click', (event) => {
-  //     const coordinateLong = event.coordinate[0];
-  //     const coordinateLat = event.coordinate[1];
-  //     console.log('Tıklanan konum:', [coordinateLong,coordinateLat]);
-  //     });
-  //     draw.on('drawend', function(event) {
-  //       var feature = event.feature;
-  //       console.log(feature);
-
-  //       var geometry = feature.getGeometry();
-
-  //      const daataa={
-  //       type:geometry.getType(),
-  //       coordinates:geometry.getCoordinates()
-  //     };
-  //     var format = new WKT();
-  //     const _wkt = format.writeGeometry(feature.getGeometry(), {
-  //       dataProjection: 'EPSG:4326',
-  //       featureProjection: 'EPSG:3857'
-  //     });
-  //     const _data2={
-  //       wkt:_wkt
-  //     }
-  //     console.log(_wkt);
-  //     // console.log(typeof(_wkt));
-  //       console.log();
-  //       _that.data=daataa;
-  //       _that.data2=_data2;
-  //       console.log(_that.data);
-
-  //       _that.locDataService.data=_that.data;
-  //       _that.locDataService.data2=_that.data2;
-
-  //       // console.log(_that.locDataService.data);
-  //       console.log(_that.locDataService.data2);
-
-  //     });
-  // }
-  // getMap(type:Type){
-  //   var raster = this.createRaster();
-  //   var source=this.createSource();
-  //   var vector=this.createVector(source);
-  //   var extent=this.createExtent();
-  //   this.map=this.createMap(raster,vector,extent);
-  //   var modify=this.createModify(source);
-  //   this.map.addInteraction(modify);
-  //   var draw = this.createDraw(source,type)
-  //   this.addInteractions(draw,source)
-  //   this.mapDraw(draw)
-  //   this.isFeatureMapActive=false;
-  // }
-  // addInteractions(draw,source) {
-  //   let snap;
-  //   // draw = new Draw({
-  //   //   source: source,
-  //   //   type: type,
-  //   // });
-  //   this.map.addInteraction(draw);
-  //   snap = new Snap({source: source});
-  //   this.map.addInteraction(snap);
-  // }
-  // createDraw(source,type):Draw{
-  //   var draw = new Draw({
-  //     source: source,
-  //     type: type,
-  //   });
-  //   return draw
-  // }
-
-  // createRaster():TileLayer<OSM>{
-  //   var raster = new TileLayer({
-  //     source: new OSM(),
-  //   });
-  //   return raster;
-  // }
-  // createMap(raster,vector,extent):Map{
-  //   var map = new Map({
-  //     layers: [raster, vector],
-  //     target: 'map',
-  //     view: new View({
-  //       center: fromLonLat([34.9998,39.42152]),
-  //       zoom: 6.8,
-  //       extent,
-  //     }),
-  //   });
-  //   return map;
-  // }
-
-  // createExtent():number[]{
-  //   var extent = get('EPSG:3857').getExtent().slice();
-  //   extent[0] += extent[0];
-  //   extent[2] += extent[2];
-  //   return extent;
-  // }
-
-  // createModify(source):Modify{
-  //   var modify = new Modify({source: source});
-  //   return modify;
-  // }
-
-  // createSource():VectorSource{
-  //   var source = new VectorSource();
-  //   return source;
-  // }
-  // createVector(source):VectorLayer<any>{
-  //   var vector = new VectorLayer({
-  //     source: source,
-  //     style: {
-  //       'fill-color': 'rgba(255, 255, 255, 0.2)',
-  //       'stroke-color': '#ffcc33',
-  //       'stroke-width': 2,
-  //       'circle-radius': 7,
-  //       'circle-fill-color': '#ffcc33',
-  //     },
-  //   });
-  //   return vector;
-  // }
-
-  // disposeMap(map:Map){
-  //   map.dispose();
-  //   this.mapİsActive=false;
-  // }
-
-  // (){
-  //   this.showSpinner();
-  //   this.httpCLient.get<LocAndUsers>({controller:"maps"}).subscribe({
-  //     next:(data)=>{
-  //       this.locDataService.listLocAndUser=data;
-  //       this.hideSpinner();
-  //       this.listModalComponent.openModal();
-  //       this._options="";
-  //     },
-  //     error:(err)=>{
-  //       alert("Bir Hata Oluştu.")
-  //     }
-  //   });
-  // }
-
-  // getGeometryBywkt(_wkt){
-  //   console.log("++++++++");
-  //   this._options="";
-  //   this.isFeatureMapActive=true
-  //   this.disposeMap(this.map);
-  //   var map;
-
-  //   const wkt = _wkt
-  //   console.log(wkt);
-
-  //   const format=new WKT();
-
-  //   // var raster = this.createRaster();
-  //   // var source=this.createSource();
-  //   const feature = format.readFeature(wkt, {
-  //     dataProjection: 'EPSG:4326',
-  //     featureProjection: 'EPSG:3857'
-  // });
-
-  //   // this.map.addLayer(vectorLayer);
-
-  //   // const feature = format.readFeature(wkt, {
-  //   //   dataProjection: 'EPSG:4326',
-  //   //   featureProjection: 'EPSG:3857',
-  //   // });
-
-  //   var geometry = feature.getGeometry() as Geometry;
-  //   // console.log(geometry)
-  //   // console.log(geometry.getCoordinates())
-  //   //  const map = new Map({
-  //   //   layers: [raster, vector],
-  //   //   target: 'map', // Harita bileşeninin ID'si
-  //   //   view: new View({
-  //   //     center: fromLonLat([34.9998,39.42152]),
-  //   //     zoom: 6.8,
-  //   //   }),
-  //   // });
-  //     this._featureMap=this.featureMap(raster,vector);
-  // }
-
-  //#region
-
   map: Map;
   vectorLayer: any;
   options = "";
@@ -303,7 +75,6 @@ export class MapComponent extends BaseComponent implements OnInit {
     this.addLayer(); //haritaya bir layer eklendi.
     // this.locDataService.veriOlusturulduSubject.subscribe((veri) => {
     //   console.log('veri geldi');
-    //   this.removeFeature();
     // });
     this.generalDataService.veriOlusturulduSubject.subscribe({
       next:()=>{
@@ -371,7 +142,7 @@ export class MapComponent extends BaseComponent implements OnInit {
     const drawInteraction = new Draw({
       type: this.generalDataService._featureType, // Çizilebilecek şekil türünü (Point, LineString, Polygon) seciyorum
     });
-    // that.map.removeInteraction(drawInteraction)
+    that.map.removeInteraction(drawInteraction)
     this.map.addInteraction(drawInteraction); //interaction i map e ekliyorum.
     // vectorLayer.getSource().clear()
     this.map.on('click', (event) => {
@@ -415,23 +186,6 @@ export class MapComponent extends BaseComponent implements OnInit {
     this.geometryListModal.openModal()
   }
 
-  // getList() {
-  //     //  const modalDiv=document.getElementById("liste")
-  //     //  if(modalDiv!=null){
-  //     //    modalDiv.style.display="block"
-  //     //  }
-  //     },
-  //     error:(err)=>{
-  //       // const modalDiv=document.getElementById("liste")
-  //       // if(modalDiv!=null){
-  //       //   modalDiv.style.display="block"
-  //       // }
-  //     }
-  //   });
-    // this.geometryListModal.getData();
-
-
-
   openDilaog(){
     this.showSpinner();
     this.httpCLientService.get<LocAndUsers>({controller:"maps"}).subscribe({
@@ -456,12 +210,6 @@ export class MapComponent extends BaseComponent implements OnInit {
 
   wktToMapFeature(_wkt){
     this.vectorLayer.getSource().clear();
-    // this.map.
-      //     var format = new WKT();
-  //     const _wkt = format.writeGeometry(feature.getGeometry(), {
-  //       dataProjection: 'EPSG:4326',
-  //       featureProjection: 'EPSG:3857'
-  //     });
     var format=new WKT();
     const feature = format.readFeature(_wkt,{
       dataProjection:'EPSG:4326',
@@ -479,14 +227,3 @@ export enum FeatureType {
   Point = 'Point',
   Polygon = 'Polygon',
 }
-// createMap(raster,vector,extent):Map{
-  //   var map = new Map({
-  //     layers: [raster, vector],
-  //     target: 'map',
-  //     view: new View({
-  //       center: fromLonLat([34.9998,39.42152]),
-  //       zoom: 6.8,
-  //       extent,
-  //     }),
-  //   });
-  //   return map;
